d82746c0085f8907b305cc021fc18d9c3fff4c27
4mater4su <robin.geske30@googlemail.com>
2025-06-20 11:39:11 +0200

    study_notes_prompt.txt

--- learnit_search.py
+++ learnit_search.py
@@ -1,10 +1,12 @@
 from learnit import LearnIt
 
-PATH_TO_PDF = "/Users/robing/Desktop/projects/Learnit/PDFs/M10_komplett.pdf"
+#PATH_TO_PDF = "/Users/robing/Desktop/projects/Learnit/PDFs/M10_komplett.pdf"
+PATH_TO_PDF = "/Users/robing/Desktop/projects/Learnit/PDFs/Schmidt-Lang-Heckmann-Physiologie-des-Menschen.pdf"
+
 
 li = LearnIt.from_pdf(PATH_TO_PDF)   # uses store “test_VS”, id saved in .vector_store_ids/test_VS.id
 
-li.search_and_copy_page(
-    query="Den Aufbau von echten Gelenken beschreiben",
-    dest_dir="/Users/robing/Desktop/projects/Learnit/testing"
+li.search_and_copy_pages(
+    query="Wie funktionieren Aktinfilamente?",
+    dest_dir="/Users/robing/Desktop/learnit_testing"
 )
